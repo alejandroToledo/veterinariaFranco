e5e4f3c295114db72aaad05b6e1e0e691b45dca5
AlejandroToledo <alejandrotol.her@gmail.com>
2020-08-07 01:57:26 -0300

    Tercer commit

--- js/data.js
+++ js/data.js
@@ -7,31 +7,37 @@ $(document).ready(
     },
   })
 );
+if ("#app") {
 
-function miPrograma(data) {
-  var app = new Vue({
-    el: "#app",
-    data: {
-      farmacia: [],
-      jugueteria: [],
-    },
-  });
-  console.log(data);
 
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].tipo === "Juguete") {
-      app.jugueteria.push(data[i]);
-    } else {
-      app.farmacia.push(data[i]);
+
+  function miPrograma(data) {
+    var app = new Vue({
+      el: "#app",
+      data: {
+        farmacia: [],
+        jugueteria: [],
+      },
+    });
+    console.log(data);
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].tipo === "Juguete") {
+        app.jugueteria.push(data[i]);
+      } else {
+        app.farmacia.push(data[i]);
+      }
     }
   }
 }
-var boton = document.getElementById("enviar");
-var desaparecer = document.getElementById("cambio");
-var aparecer = document.getElementById("pata");
 
-console.log(boton);
-boton.addEventListener("click", function () {
-  desaparecer.style.display = "none";
-  aparecer.style.display = "flex";
-});
+if ("#contacto") {
+  var boton = document.getElementById("enviar");
+  var desaparecer = document.getElementById("cambio");
+  var aparecer = document.getElementById("pata");
+
+  boton.addEventListener("click", function () {
+    desaparecer.style.display = "none";
+    aparecer.style.display = "flex";
+  });
+}
