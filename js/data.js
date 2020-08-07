e0f770235bfe72f965c30d886e269b967c9b21ae
AlejandroToledo <alejandrotol.her@gmail.com>
2020-08-06 23:09:23 -0300

    Segundo commit

--- js/data.js
+++ js/data.js
@@ -13,16 +13,25 @@ function miPrograma(data) {
     el: "#app",
     data: {
       farmacia: [],
-      jugueteria: []
+      jugueteria: [],
     },
   });
-  console.log(data)
+  console.log(data);
 
   for (let i = 0; i < data.length; i++) {
     if (data[i].tipo === "Juguete") {
-      app.jugueteria.push(data[i])
+      app.jugueteria.push(data[i]);
     } else {
-      app.farmacia.push(data[i])
+      app.farmacia.push(data[i]);
     }
   }
 }
+var boton = document.getElementById("enviar");
+var desaparecer = document.getElementById("cambio");
+var aparecer = document.getElementById("pata");
+
+console.log(boton);
+boton.addEventListener("click", function () {
+  desaparecer.style.display = "none";
+  aparecer.style.display = "flex";
+});
